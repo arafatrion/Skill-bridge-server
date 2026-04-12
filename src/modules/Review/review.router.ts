de85158504cdb7d0720586617580a5c46d6f3185
Arafatrion <aislam210030@gmail.com>
2026-04-12 10:46:26 +0600

    add review route with auth

---
+++ src/modules/Review/review.router.ts
@@ -0,0 +1,9 @@
+import express from "express"
+import auth, { userRole } from "../../middlewares/auth";
+import { ReviewController } from "./review.controller";
+
+const router = express.Router();
+
+router.post("/",auth(userRole.student),ReviewController.createReview)
+
+export const ReviewRouter = router;
