84df74185bdbbfff3f5c4ccd42b4649d8f063ae6
Arafatrion <aislam210030@gmail.com>
2026-04-12 10:52:45 +0600

    feat: implement review fetch and delete routes

--- src/modules/Review/review.router.ts
+++ src/modules/Review/review.router.ts
@@ -4,6 +4,8 @@ import { ReviewController } from "./review.controller";
 
 const router = express.Router();
 
-router.post("/",auth(userRole.student),ReviewController.createReview)
+router.post("/",auth(userRole.student),ReviewController.createReview);
+router.get("/:tutorId",ReviewController.getTutorReviews);
+router.delete("/:id",auth(userRole.admin),ReviewController.deleteReview)
 
 export const ReviewRouter = router;
